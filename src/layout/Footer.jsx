2e542157482f98bd0fc2699fc20bb495adc53af2
Morgan Strohl <morgan.strohl@gmail.com>
2023-06-30 13:12:23 +0200

    commit -m "FOOTER - Suppression du texte et modification CSS padding et margin"

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,28 +1,25 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 import "../styles/Footer.css";
 
 function Footer() {
   return (
     <div className="footer">
-       <ul>
+      <ul>
         <Link to="Contact">
-        <li>
-          Nous contacter
-        </li>
+          <li>
+            Nous contacter
+          </li>
         </Link>
         <Link to="Mentions">
-        <li>
-          Mentions légales
-        </li>
+          <li>
+            Mentions légales
+          </li>
         </Link>
       </ul>
       <div className="formulaireDeContact">
         <p>Les BÔ Bro</p>
-        <p> 76 Rue de la Clef des Champs 
-            95180 Menucourt
-        </p>
       </div>
       <div className="Copyright">
         <p> CCR 2023 - Tous droits réservés</p>
