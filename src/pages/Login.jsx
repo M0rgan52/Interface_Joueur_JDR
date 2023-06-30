520dbde5142e43ca0c3d424a54eb02b6aca014f3
Morgan Strohl <morgan.strohl@gmail.com>
2023-06-30 11:03:07 +0200

    commit -m "PAGE LOG - Changement du titre h1"

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -6,17 +6,17 @@ import "../styles/Login.css"
 
 function Home() {
     return (
-   
+
         <div className='main'>
-                <header>
-                <h1>Les BÔ Bro !</h1>
+            <header>
+                <h1>Interface joueur JDR</h1>
                 <p>Bienvenue jeunes joueurs ! Vous êtes passionnés par les jeux de rôles? Vous avez envie de créer vos propres fiches de personnages? Vous êtes au bon endroit</p>
-                </header>
-                <Inscription/>
-                
-               
+            </header>
+            <Inscription />
+
+
         </div>
-        
+
     )
 }
 
